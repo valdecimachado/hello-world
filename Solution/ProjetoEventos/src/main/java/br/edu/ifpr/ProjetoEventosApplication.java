c6c59a7adedf53a860b76962ac9b00809c86f240
valdeci <valdecimac@gmail.com>
2019-09-26 21:58:09 -0300

    Criação do projeto

---
+++ Solution/ProjetoEventos/src/main/java/br/edu/ifpr/ProjetoEventosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEventosApplication.class, args);
+	}
+
+}
