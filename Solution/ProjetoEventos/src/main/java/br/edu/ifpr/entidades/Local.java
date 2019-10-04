be6fe36ef8d0b4336e24c784b91bfb5f35dbb1b4
valdeci <valdecimac@gmail.com>
2019-10-03 22:15:24 -0300

    Criado atributos da classe Local

--- Solution/ProjetoEventos/src/main/java/br/edu/ifpr/entidades/Local.java
+++ Solution/ProjetoEventos/src/main/java/br/edu/ifpr/entidades/Local.java
@@ -1,5 +1,9 @@
 package br.edu.ifpr.entidades;
 
 public class Local {
+	private String descricao;
+	private String endereco;
+	private Integer capacidadePublico;
+	private Boolean habilitado; 
 
 }
